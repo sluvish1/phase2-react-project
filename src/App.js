d5c7581664bbadd3057eb1b3a822dfda45f2d92b
Vaschell Mkenzie <mvaschell@gmail.com>
2023-06-16 19:54:56 -0400

    idk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import NavBar from "./NavBar";
+import HomePage from "./Homepage";
+import BooksList from "./BooksList";
+import BooksToRead from "./BooksToRead";
+import BooksRead from "./BooksRead";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>The BookClub</h1>
+      <NavBar />
+      <Switch>
+        <Route path="/bookslist">
+          <BooksList />
+        </Route>
+        <Route path="/readlater">
+          <BooksToRead />
+        </Route>
+        <Route path="/booksread">
+          <BooksRead />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
