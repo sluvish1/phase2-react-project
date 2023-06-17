8ee0338ae8635150ece4d14d8e2e443889976bd2
Vaschell Mkenzie <mvaschell@gmail.com>
2023-06-16 21:23:19 -0400

    created a form

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,11 +2,30 @@ import React from "react";
 
 
 function HomePage() {
-    return (
-        <main>
 
-        </main>
-    )
+
+
+  return (
+      <form className="NewBook">
+        <label>
+           Book Review: 
+          <input type="text" placeholder=" Enter Book Author"></input>
+          <input type="text" placeholder=" Enter Book Title"></input>
+          <input type="summury" placeholder=" Enter Book Summury"></input>
+         </label>
+              
+        <label>
+          Book Category:
+          <select name=" book category">
+            <option value="Fantasy">Fantasy</option>
+            <option value="Anime">Anime</option>
+            <option value="Drama">Drama</option>
+            <option value="Triller">Triller</option>
+          </select>
+        </label>
+        <button type="submit">Submit Book</button>
+      </form>
+  );
 }
 
-export default HomePage
+export default HomePage;
